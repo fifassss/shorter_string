6ad7c6bec116ab933ab4f8044f7c56f3e685a09a
fifassss <133858543+fifassss@users.noreply.github.com>
2025-01-05 02:51:12 +0700

    get files \ добавил файлы
    загрузил проект

---
+++ Projectone/Projectone/Source.cpp
@@ -0,0 +1,57 @@
+// подключение дериктивы для стандарта ввода \ вывода
+#include <iostream> 
+#include <string>
+
+int main()
+{
+    std::cout << "hey, i programm,the string shortener,if you want leave from programm,please write 'l'." << '\n';
+
+    while (true)
+    {
+
+        std::string worked{};
+
+        int num_one{};
+
+        int num_two{};
+
+        std::cout << "please,write something." << '\n';
+
+        std::cin >> worked;
+
+        if (worked == "l")
+        {
+
+            break;
+
+        }
+
+        std::cout << "now,please,write num one,that number means, where to start short string." << '\n';
+        
+        std::cin >> num_one;
+
+        if (num_one == 'l')
+        {
+
+            break;
+
+        }
+
+        std::cout << "now,please,write num two,that number means, how many symbols get." << '\n';
+
+        std::cin >> num_two;
+
+        if (num_two == 'l')
+        {
+
+            break;
+
+        }
+
+        std::cout << worked.substr(num_one, num_two) << '\n';
+
+    }
+
+    return 0;
+
+}
